a98f74037d1e7f0058ddb93e1a84447ffada0b05
Deven Bhimani <92947650+Deven213@users.noreply.github.com>
2024-03-29 23:15:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
-import {fileURLToPath} from 'url';
+
+
 
 //configure env
 dotenv.config();
@@ -16,9 +17,7 @@ dotenv.config();
 //databse config
 connectDB();
 
-//esmodeule fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 
 //rest object
 const app = express();
