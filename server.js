f6f6d72366b18a4dead1ace18c4c72b6021d0f2b
deven <devenbhimani123@gmail.com>
2024-03-29 21:52:58 +0530

    update the code

--- server.js
+++ server.js
@@ -27,7 +27,6 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-
 //rest api
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to ecommerce app</h1>");
@@ -36,10 +35,10 @@ app.get("/", (req, res) => {
 //PORT
 const PORT = process.env.PORT || 8080;
 
-// //run listen
-// app.listen(PORT, () => {
-//   console.log(
-//     `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
-//       .white
-//   );
-// });
+//run listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
+});
